2435c3cc18abb0fe707ae55fb231a0ab55144af2
Tope <temitopeadebawojo@gmail.com>
2026-06-24 03:42:55 +0100

    calculator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,89 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState } from "react"
+
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
+export default function calculator() {
+
+  const [value, setValue] = useState("0");
+
+  const handlekey = (key) => {
+    if (value === "0") {
+      setValue(key);
+    } else {
+      setValue(value + key);
+    }
+  }
+
+  const handleClear = () => {
+    setValue("0")
+  }
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div >
+      <div className="min-h-screen w-full bg-gradient-to-br from-slate-950 via-slate-900 to-purple-950/40 flex items-center justify-center p-4">
+
+        <div>
+          <Card className='w-80 rounded-2xl bg-slate-950/70 backdrop-blur-3xl border border-white/10 inset-shadow-purple-950/30 shadow-2xl shadow-black/70 '>
+            <CardHeader>
+              <CardTitle className='uppercase text-sm tracking-widest text-slate-500'>Tope&apos;s calc</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <input type='text'
+                value={value}
+                readOnly
+                className='w-full h-20 bg-slate-800/80 backdrop-blur-3xl rounded-xl text-slate-300 text-3xl text-right pr-4' />
+
+              <div className='flex grid grid-cols-4 gap-4 pt-6 '>
+                <button onClick={handleClear} className='col-span-3 bg-red-600/90 hover:bg-red-800/90 hover:text-red-300 text-red-100 p-2 rounded-lg '>C</button>
+
+                <button onClick={() => handlekey("/")} className='bg-amber-600/90 hover:bg-amber-800/90 hover:text-amber-300 text-amber-100 p-2 rounded-lg '>/</button>
+
+                <button onClick={() => handlekey("7")} className='bg-black/90 hover:bg-gray-950/90 hover:text-gray-400 text-slate-100 p-2 rounded-lg '>7</button>
+
+                <button onClick={() => handlekey("8")} className='bg-black/90 hover:bg-gray-950/90 hover:text-gray-400 text-slate-100 p-2 rounded-lg '>8</button>
+
+                <button onClick={() => handlekey("9")} className='bg-black/90 hover:bg-gray-950/90 hover:text-gray-400 text-slate-100 p-2 rounded-lg '>9</button>
+
+                <button onClick={() => handlekey("*")} className='bg-amber-600/90 hover:bg-amber-800/90 hover:text-amber-300 text-amber-100 p-2 rounded-lg '>*</button>
+
+                <button onClick={() => handlekey("4")} className='bg-black/90 hover:bg-gray-950/90 hover:text-gray-400 text-slate-100 p-2 rounded-lg '>4</button>
+
+                <button onClick={() => handlekey("5")} className='bg-black/90 hover:bg-gray-950/90 hover:text-gray-400 text-slate-100 p-2 rounded-lg '>5</button>
+
+                <button onClick={() => handlekey("6")} className='bg-black/90 hover:bg-gray-950/90 hover:text-gray-400 text-slate-100 p-2 rounded-lg '>6</button>
+
+                <button onClick={() => handlekey("-")} className='bg-amber-600/90 hover:bg-amber-800/90 hover:text-amber-300 text-amber-100 p-2 rounded-lg '>-</button>
+
+                <button onClick={() => handlekey("1")} className='bg-black/90 hover:bg-gray-950/90 hover:text-gray-400 text-slate-100 p-2 rounded-lg '>1</button>
+
+                <button onClick={() => handlekey("2")} className='bg-black/90 hover:bg-gray-950/90 hover:text-gray-400 text-slate-100 p-2 rounded-lg '>2</button>
+
+                <button onClick={() => handlekey("3")} className='bg-black/90 hover:bg-gray-950/90 hover:text-gray-400 text-slate-100 p-2 rounded-lg '>3</button>
+
+                <button onClick={() => handlekey("+")} className='bg-amber-600/90 hover:bg-amber-800/90 hover:text-amber-300 text-amber-100 p-2 rounded-lg '>+</button>
+
+                <button onClick={() => handlekey("0")} className='col-span-2 bg-black/90 text-slate-100 hover:bg-gray-950/90 hover:text-gray-400 p-2 rounded-lg '>0</button>
+
+                <button onClick={() => handlekey(".")} className='bg-black/90 text-slate-100 hover:bg-gray-950/90 hover:text-gray-400 p-2 rounded-lg '>.</button>
+
+                <button className='bg-green-600/90 text-slate-100 p-2 rounded-lg '>=</button>
+              </div>
+            </CardContent>
+          </Card>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+
+      </div>
     </div>
-  );
+  )
 }
